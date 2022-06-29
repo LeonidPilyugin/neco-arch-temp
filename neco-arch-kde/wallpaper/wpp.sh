3dd3a865d754760f304aa8f5993ddf5c2aba6829
Leonid Pilyugin <l.pilyugin04@gmail.com>
2022-06-29 09:18:51 +0300

    AMOGUS

---
+++ neco-arch-kde/wallpaper/wpp.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo pacman -S python-pip --noconfirm
+pip install dbus-python
+
+
+
+CRL=$(curl https://raw.githubusercontent.com/pashazz/ksetwallpaper/master/ksetwallpaper.py)
+python -c "$CRL" --file "$PWD/wpl.png"  --lock-screen
+python -c "$CRL" --file "$PWD/wp.png"
